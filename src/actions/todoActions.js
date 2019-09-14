8d9b101c9a04d0a85e3df2e95220e8a630c448b4
Anna <anna.sabransky@gmail.com>
2019-09-14 18:33:27 +0200

    style guide

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 export const fetchTodos = () => dispatch => {
     console.log('fetching');
     axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      //.then(res => res.json())
       .then(res => {console.log(res.data)
         dispatch({
           type: FETCH_TODOS,
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,30 +1,28 @@
 import React, {Component} from 'react';
 import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import { connect } from 'react-redux';
 import { fetchTodos } from '../actions/todoActions';
 
 class Todos extends Component {  
     componentDidMount() {
-        this.props.fetchTodos();
-      }
+      this.props.fetchTodos();
+    }
 
-    
     render() {
-        console.log(this.props.todos)
-        return this.props.todos.map((todo) => (
-            <TodoItem key={todo.id} todo={todo}/>
+      console.log(this.props.todos)
+      return this.props.todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo}/>
     ))
   }
 }
 
 Todos.propTypes = {
-    todos: PropTypes.array.isRequired,
+  todos: PropTypes.array.isRequired,
 }
 
 const mapStateToProps = state => ({
-    todos: state.todos,
+  todos: state.todos,
 })
 
 export default connect(mapStateToProps, { fetchTodos}) (Todos);
